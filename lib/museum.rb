e251b1f05967aea21f076d22277141d6c0214cac
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-10 10:15:46 -0600

    Add recommend exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,15 @@ class Museum
     @exhibits << feature
   end
 
+  def recommend_exhibits(patron)
+    you_might_like = []
+    patron.interests.map do |interest|
+      @exhibits.map do |exhibit|
+        if exhibit.name == interest
+          you_might_like << exhibit
+        end
+      end
+    end
+    you_might_like.flatten
+  end
 end
